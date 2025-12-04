23c09fde7ea47d106d8f8011ce3045017c68f29a
GushchinAndrei1 <andreyka.gushchin.03@mail.ru>
2025-12-04 22:50:42 +0500

    реализация func_array

--- 02_week/tasks/func_array/func_array.cpp
+++ 02_week/tasks/func_array/func_array.cpp
@@ -1,27 +1,28 @@
 #include <stdexcept>
 
-
 double ApplyOperations(
     double a,
     double b,
-    double (*operations[])(double, double),
-    size_t size
+    double (*mathOperations[])(double, double), // массив указателей, который принимает два числа типа double
+    size_t size // рамзер массива
 ) {
+    // если массив пуст - возвращаем 0.0
     if (size == 0) {
         return 0.0;
     }
-
+    // переменная для суммы мат операций
     double sum = 0.0;
-
+    // проходим по каждому элементу массива 
     for (size_t i = 0; i < size; ++i) {
 
-        // ---- ключевая защита ----
-        if (operations[i] == nullptr) {
+        // если один из указателей массива пустой, то пропускаем
+        if (mathOperations[i] == nullptr) {
             continue; // пропускаем пустую функцию
         }
-
-        double value = operations[i](a, b);
-        sum += value;
+        // вызываем i-ю функцию с числами a и b, кладем в v
+        double v = mathOperations[i](a, b);
+        // прибавляем к общей сумме
+        sum += v;
     }
 
     return sum;
