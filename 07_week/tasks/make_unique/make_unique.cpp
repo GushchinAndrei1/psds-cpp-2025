2281043527a3412382f237c5adc948325e2c5696
GushchinAndrei1 <andreyka.gushchin.03@mail.ru>
2026-03-10 19:02:34 +0500

    Реализация make_unique

--- 07_week/tasks/make_unique/make_unique.cpp
+++ 07_week/tasks/make_unique/make_unique.cpp
@@ -1,4 +1,10 @@
 #include <memory>
+#include <utility>
 
-
-/* return_type */ MakeUnique( /* args */ );
+// Создаёт объект типа T в динамической памяти
+// и возвращает std::unique_ptr на него.
+// Аргументы конструктора передаются через perfect forwarding.
+template <typename T, typename... Args>
+std::unique_ptr<T> MakeUnique(Args&&... args) {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
